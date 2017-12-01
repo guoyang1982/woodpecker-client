d0aa39323532f9c2375215937f9892fd642662be
guoyang9 <guoyang9@le.com>
2017-12-01 14:57:05 +0800

    提交 增加控制日志类操作

--- wpclient-agent-core/src/main/java/com/gy/woodpecker/netty/NettyTelnetHandler.java
+++ wpclient-agent-core/src/main/java/com/gy/woodpecker/netty/NettyTelnetHandler.java
@@ -37,7 +37,11 @@ public class NettyTelnetHandler extends SimpleChannelInboundHandler<String> {
         }else if ("help".equals(request.toLowerCase())){
             response = "==================命令解释======================\r\n" +
                     "命令:m t\r\n监听发送日志消息线程池相关信息!\r\n命令:close hc\r\n关闭健康检查!\r\n命令:open hc\r\n打开健康检查!\r\n" +
-                    "命令:close log\r\n关闭日志监控,不发送日志消息!\r\n命令:open log\r\n打开日志监控,发送日志消息!\r\n命令:help\r\n帮助命令!\r\n" +
+                    "命令:close log\r\n关闭日志监控,不发送日志消息!\r\n命令:open log\r\n打开日志监控,发送日志消息!\r\n" +
+                    "命令:log info\r\n获取应用日志类命令,可以过滤制定某些包下的类 如:log info com.!\r\n" +
+                    "命令:log level 参数1 参数2\r\n设置应用类的日志级别,如 log level com. debug." +
+                    "也可以设置root级别如 log level root debug,支持error warn info debug\r\n" +
+                    "命令:help\r\n帮助命令!\r\n" +
                     "===============================================\r\n";
         } else if ("bye".equals(request.toLowerCase())) {
             response = "退出客户端!\r\n";
@@ -56,7 +60,31 @@ public class NettyTelnetHandler extends SimpleChannelInboundHandler<String> {
         } else if ("open log".equals(request.toLowerCase())){
             LoggerFacility.f = true;
             response = "成功打开日志监控!\r\n";
-        } else{
+        } else if (request.toLowerCase().startsWith("log info")){
+            String[] loginfo = request.split(" ");
+            String value = "";
+            if(loginfo.length>2){
+                value = loginfo[2];
+            }
+            response = LoggerFacility.getLogInfor(value)+"\r\n";
+        } else if (request.toLowerCase().startsWith("log level ")){
+            String[] loginfo = request.split(" ");
+            String className = "";
+            String level = "";
+            if(loginfo.length == 4){
+                className = loginfo[2];
+                level = loginfo[3];
+                //ERROR、WARN、INFO、DEBUG
+                if(level.toLowerCase().equals("error") || level.toLowerCase().equals("warn") || level.toLowerCase().equals("info") || level.toLowerCase().equals("debug")){
+                    LoggerFacility.setLogLevel(className,level);
+                    response = "设置日志成功!\r\n";
+                }else{
+                    response = "您输入的命令参数错误!\r\n";
+                }
+            }else{
+                response = "您输入的命令参数错误!\r\n";
+            }
+        }else{
             response = "您输入的命令不存在 '" + request + "'!\r\n";
         }
 
